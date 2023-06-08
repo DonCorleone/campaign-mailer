158f5f34ee3e5568183a17a0faf59d5d2e6d3001
DonCorleone <linus@wieland.click>
2023-06-08 23:13:22 +0200

    gmail friendly

--- sendMails.py
+++ sendMails.py
@@ -1,3 +1,4 @@
+import base64
 import configparser
 import csv
 from email.mime.image import MIMEImage
@@ -25,68 +26,69 @@ token = config.get('Credentials', 'token')
 # Open the image files and read their contents
 with open("image1.jpg", "rb") as f:
     img1_data = f.read()
+   # img_base64 = base64.b64encode(img1_data).decode("utf-8")
+    # Create image attachment objects
+    img1 = MIMEImage(img1_data)
 
-# Create image attachment objects
-img1 = MIMEImage(img1_data)
+    img1.add_header("Content-Disposition", "inline", filename="image1.jpg")
+    # Set the content IDs of the image attachments
+    img1.add_header("Content-ID", "image1")
 
-# Set the content IDs of the image attachments
-img1.add_header("Content-ID", "image1")
+    postmark = PostmarkClient(server_token=token)
 
-postmark = PostmarkClient(server_token=token)
+    # collect all the emails in a list
+    emailsRaw = []
+        
+    # load the csv file
+    with open('mails.csv', newline='') as csvfile:
+        # open a html file and read the html content
+        with open('mail.html', 'r') as htmlfile:
+            html = htmlfile.read()
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in reader:
+                # skip the first line
+                if reader.line_num == 1:
+                    continue
+                # skip empty lines
+                if row[0] == '':
+                    continue
+                # skip lines that do not have the right length
+                if len(row) != 4:
+                    continue
+                # skip lines that do not have a valid mail address
+                if '@' not in row[1]:
+                    continue
+                # skip lines that do not have a valid name
+                if row[2] == '':
+                    continue
+                # skip lines that do not have a valid surname
+                if row[3] == '':
+                    continue
 
-# collect all the emails in a list
-emailsRaw = []
-    
-# load the csv file
-with open('mails.csv', newline='') as csvfile:
-    # open a html file and read the html content
-    with open('mail.html', 'r') as htmlfile:
-        html = htmlfile.read()
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in reader:
-            # skip the first line
-            if reader.line_num == 1:
-                continue
-            # skip empty lines
-            if row[0] == '':
-                continue
-            # skip lines that do not have the right length
-            if len(row) != 4:
-                continue
-            # skip lines that do not have a valid mail address
-            if '@' not in row[1]:
-                continue
-            # skip lines that do not have a valid name
-            if row[2] == '':
-                continue
-            # skip lines that do not have a valid surname
-            if row[3] == '':
-                continue
+                # create the email
+                emailRaw = {
+                    'From': 'linus@schlosswochen.ch',
+                    'To': row[2] + ' ' + row[3] + ' <' + row[1] + '>',
+                    'Surname' : row[2],
+                }
 
-            # create the email
-            emailRaw = {
-                'From': 'linus@schlosswochen.ch',
-                'To': row[2] + ' ' + row[3] + ' <' + row[1] + '>',
-                'Surname' : row[2],
-            }
+                emailsRaw.append(emailRaw)
 
-            emailsRaw.append(emailRaw)
+    emailList = list()
+    for emailRaw in emailsRaw:
+        # create the email-dictionary
+        emailDict = {
+            "From": emailRaw['From'],
+            "To": emailRaw['To'],
+            "TemplateId": 32013601,
+            "TemplateModel": {
+                "fizz": emailRaw['Surname']
+            },
+            "Attachments": [img1]
+        }
 
-emailList = list()
-for emailRaw in emailsRaw:
-    # create the email-dictionary
-    emailDict = {
-        "From": emailRaw['From'],
-        "To": emailRaw['To'],
-        "TemplateId": 32013601,
-        "TemplateModel": {
-            "fizz": emailRaw['Surname']
-        },
-        "Attachments": [img1]
-    }
+        # add the emailDict to the list of dictionaries
+        emailList.append(emailDict)
 
-    # add the emailDict to the list of dictionaries
-    emailList.append(emailDict)
-
-response = postmark.emails.send_template_batch(*emailList)
-print(f"Response: {response}")
+    response = postmark.emails.send_template_batch(*emailList)
+    print(f"Response: {response}")
